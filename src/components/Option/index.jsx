1d9dc59380ca10657c6c40f1cd79684797154087
JanaEsther <j.obendraufova@h2o-logic.cz>
2024-10-27 21:29:12 +0100

    handleClick, onSelected

--- src/components/Option/index.jsx
+++ src/components/Option/index.jsx
@@ -1,9 +1,9 @@
 import { Icon } from '../Icon/index';
 import './style.css';
 
-export const Option = ({ type, text }) => {
+export const Option = ({ type, text, onSelected }) => {
   return (
-    <div className="option" onClick={console.log('Spíše souhlasím.')}>
+    <div className="option" onClick={() => onSelected(type)}>
       <Icon type={type} />
       <div>{text}</div>
     </div>
--- src/components/Question/index.jsx
+++ src/components/Question/index.jsx
@@ -3,22 +3,26 @@ import { QuestionBody } from '../QuestionBody/index';
 import { Option } from '../Option/index';
 import './style.css';
 
-export const Question = ({text}) => {
-  const[answered, setAnswered]= useState(false)
+export const Question = ({ text }) => {
+  const [answered, setAnswered] = useState(false);
 
-  
+  const handleClick = () => {
+    setAnswered((prevState) => !prevState);
+  };
+
   return (
     <div className="question">
-      <QuestionBody 
-        iconType="symbolQuestion" 
+      <QuestionBody
+        iconType={`icon ${answered ? 'symbolQuestion' : 'symbolTick'}`}
+        onClick={() => {handleClick}}
         text={text}
       />
       <div className="question__options">
-        <Option type="smileyStrongYes" text="Souhlasím"/>
-        <Option type="smileyYes" text="Spíše souhlasím"/>
-        <Option type="smileyNeutral" text="Nevím"/>
-        <Option type="smileyNo" text="Spíše nesouhlasím"/>
-        <Option type="smileyStrongNo" text="Nesouhlasím"/>
+        <Option type="smileyStrongYes" text="Souhlasím" />
+        <Option type="smileyYes" text="Spíše souhlasím" />
+        <Option type="smileyNeutral" text="Nevím" />
+        <Option type="smileyNo" text="Spíše nesouhlasím" />
+        <Option type="smileyStrongNo" text="Nesouhlasím" />
       </div>
     </div>
   );
